e241b19e55feb083ed5d084e3f317ffafd7e1b81
Charles Samborski <demurgos@demurgos.net>
2018-10-26 12:49:03 +0200

    [all] Enable more tests

--- test/build-test.sh
+++ test/build-test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
-cd ../swf-parser.rs && cargo build
-cd ../test && RUST_BACKTRACE=1 python3 ./test.py
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
+set -x
+cd "${PROJECT_ROOT}"
+(cd swf-parser.rs && cargo build)
+(cd swf-parser.ts && npm run prestart)
+node test/test.js
