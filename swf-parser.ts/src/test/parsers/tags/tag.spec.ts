c68b4252b81351f72f3d82817a8354e80977506c
Charles Samborski <demurgos@demurgos.net>
2019-01-21 00:42:39 +0100

    [ts] Add `parseTag` test

--- swf-parser.ts/src/test/parsers/tags/tag.spec.ts
+++ swf-parser.ts/src/test/parsers/tags/tag.spec.ts
@@ -1,8 +1,9 @@
 import chai from "chai";
 import { JsonValueReader } from "kryo/readers/json-value";
 import { JsonValueWriter } from "kryo/writers/json-value";
-import { $PlaceObject, PlaceObject } from "swf-tree/tags";
-import { parsePlaceObject3 } from "../../../lib/parsers/tags";
+import { $Tag, Tag } from "swf-tree/tag";
+import { DefaultParseContext } from "../../../lib/parse-context";
+import { parseTag } from "../../../lib/parsers/tags";
 import { Stream } from "../../../lib/stream";
 import { readTestJson } from "../../_utils";
 import { readStreamJson, StreamJson } from "../_utils";
@@ -10,11 +11,11 @@ import { readStreamJson, StreamJson } from "../_utils";
 const JSON_VALUE_READER: JsonValueReader = new JsonValueReader();
 const JSON_VALUE_WRITER: JsonValueWriter = new JsonValueWriter();
 
-describe("tags.parsePlaceObject3", function () {
+describe("tags.parseTag", function () {
   interface Item {
     input: Stream;
     expected: {
-      result: PlaceObject;
+      result: Tag;
       stream: Stream;
     };
   }
@@ -27,13 +28,13 @@ describe("tags.parsePlaceObject3", function () {
     };
   }
 
-  const itemsJson: ItemJson[] = readTestJson("parsers/tags/place-object3.json") as ItemJson[];
+  const itemsJson: ItemJson[] = readTestJson("parsers/tags/tag.json") as ItemJson[];
   const items: Item[] = [];
   for (const itemJson of itemsJson) {
     items.push({
       input: readStreamJson(itemJson.input),
       expected: {
-        result: $PlaceObject.read(JSON_VALUE_READER, itemJson.expected.result),
+        result: $Tag.read(JSON_VALUE_READER, itemJson.expected.result),
         stream: readStreamJson(itemJson.expected.stream),
       },
     });
@@ -41,14 +42,14 @@ describe("tags.parsePlaceObject3", function () {
 
   for (let i: number = 0; i < items.length; i++) {
     const item: Item = items[i];
-    it(`Should parse the PlaceObject tag in the test case ${i}`, function () {
-      const actual: PlaceObject = parsePlaceObject3(item.input, 8);
+    it(`Should parse the Tag in the test case ${i}`, function () {
+      const actual: Tag = parseTag(item.input, new DefaultParseContext(8));
       // console.warn("Ignoring equality test due to floats");
       chai.assert.deepEqual(
-        $PlaceObject.write(JSON_VALUE_WRITER, actual),
-        $PlaceObject.write(JSON_VALUE_WRITER, item.expected.result),
+        $Tag.write(JSON_VALUE_WRITER, actual),
+        $Tag.write(JSON_VALUE_WRITER, item.expected.result),
       );
-      chai.assert.isTrue($PlaceObject.equals(actual, item.expected.result));
+      chai.assert.isTrue($Tag.equals(actual, item.expected.result));
       chai.assert.isTrue(Stream.equals(item.input.tail(), item.expected.stream));
     });
   }
